99f427669b4fe54c5dadd439e77b29215a6e43b9
Rafaela-Quinzel <71155947+Rafaela-Quinzel@users.noreply.github.com>
2020-10-05 23:42:39 -0300

    Atividade de funções concluída. Tentei fazer os desafios, porém não consegui

--- semana4/funcoes/index.js
+++ semana4/funcoes/index.js
@@ -40,95 +40,94 @@ minhaFuncao(10) */
 //EXERCÍCIO 4 - Escrita de código:
 
 //   //---> Resposta a.:
-//     function printPresentation(){
-//         console.log("Eu sou Rafaela, tenho 30 anos, moro no Rio Grande do Sul e sou estudante.")
-//     }
-//     printPresentation()
+    function printPresentation(){
+        console.log("Eu sou Rafaela, tenho 30 anos, moro no Rio Grande do Sul e sou estudante.")
+    }
+    printPresentation()
 
-//     //---> Resposta b.:
-//     function printMessage(questions){
-//         let yesNo
-//         if(questions === "sim"){
-//            yesNo = "sou estudante"
-//         } else {
-//            yesNo = "não sou estudante"
-//         }
-//         return yesNo
-//     }
-//     const nameUser = prompt("Qual é o seu nome?")
-//     const ageUser = Number(prompt("Qual a sua idade?"))
-//     const cityUser = prompt("Em qual cidade você mora?")
-//     const askUser = prompt("Você é estudante?").toLocaleLowerCase()
-//     const answerUser = printMessage(askUser)
+    //---> Resposta b.:
+    function printMessage(questions){
+        let yesNo
+        if(questions === "sim"){
+           yesNo = "sou estudante"
+        } else {
+           yesNo = "não sou estudante"
+        }
+        return yesNo
+    }
+    const nameUser = prompt("Qual é o seu nome?")
+    const ageUser = Number(prompt("Qual a sua idade?"))
+    const cityUser = prompt("Em qual cidade você mora?")
+    const askUser = prompt("Você é estudante?").toLocaleLowerCase()
+    const answerUser = printMessage(askUser)
     
-//     console.log(`Eu sou ${nameUser}, tenho ${ageUser} anos, moro em ${cityUser} e ${answerUser}.`)
+    console.log(`Eu sou ${nameUser}, tenho ${ageUser} anos, moro em ${cityUser} e ${answerUser}.`)
  
 
 //EXERCÍCIO 5 - Escrita de código:
 
 //   //---> Resposta a.:
-//     let sumTwoNumbers = (number1, number2) => {
-//         const sum = number1 + number2
-//         return sum
-//     }
-//     const totalSum = sumTwoNumbers(23, 52)
-//     console.log(totalSum)
+    let sumTwoNumbers = (number1, number2) => {
+        const sum = number1 + number2
+        return sum
+    }
+    const totalSum = sumTwoNumbers(23, 52)
+    console.log(totalSum)
   
-//   //---> Resposta b.:  
-//     let comparesTwoNumbers = (number1, number2) => {
-//         let result
-//         if(number1 > number2){
-//           result = "O primeiro número é maior que o segundo!"
-//         } else if(number1 < number2){
-//             result = "O segundo número é maior que o primeiro!"
-//         } else{
-//             result = "O primeiro número e o segundo número são iguais!"
-//         }
-//         return result
-//     }
-//     const resultCompares = comparesTwoNumbers(23, 52)
-//     console.log(resultCompares)
+  //---> Resposta b.:  
+    let comparesTwoNumbers = (number1, number2) => {
+        let result
+        if(number1 > number2){
+          result = "O primeiro número é maior que o segundo!"
+        } else if(number1 < number2){
+            result = "O segundo número é maior que o primeiro!"
+        } else{
+            result = "O primeiro número e o segundo número são iguais!"
+        }
+        return result
+    }
+    const resultCompares = comparesTwoNumbers(23, 52)
+    console.log(resultCompares)
 
-// //---> Resposta c.:  
-//     let repeatMessage = () => {
-//         for(let i = 0; i < 10; i++){
-//          console.log("Sou estudante da Labenu!");
-//         }
-//     }
-//     repeatMessage()
+//---> Resposta c.:  
+    let repeatMessage = () => {
+        for(let i = 0; i < 10; i++){
+         console.log("Sou estudante da Labenu!");
+        }
+    }
+    repeatMessage()
 
 
-// //EXERCÍCIO 6 - Escrita de código:
+//EXERCÍCIO 6 - Escrita de código:
  const array = [10, 23, 45, 78, 90, 52, 35, 67, 84, 22]
 
-//  //---> Resposta a.:
-//  let printQuantity = (arrayNumbers) => {
-//      for(let i = 0; i < array.length; i++){
-//          console.log(array[i])
-//      }
-//        return array
-//  }
-//  let quantityElements = printQuantity()
-//  console.log(quantityElements)
+ //---> Resposta a.:
+ let printQuantity = (arrayNumbers) => {
+     for(let i = 0; i < array.length; i++){
+         console.log(array[i])
+     }
+       return array
+ }
+ let quantityElements = printQuantity()
+ console.log(quantityElements)
 
 //---> Resposta b.:
-// let checkNumber = (number) => {
-//     let resultNumber
-//     if(number % 2 === 0){
-//     resultNumber = "O número é PAR!"
-//     } else{
-//         resultNumber = "O número é IMPAR!"
-//     }
-//   return resultNumber
-// }
-// const resultCheck = checkNumber(78)
-// console.log(resultCheck)
+ let checkNumber = (number) => {
+    let resultNumber
+    if(number % 2 === 0){
+    resultNumber = "O número é PAR!"
+    } else{
+        resultNumber = "O número é IMPAR!"
+    }
+  return resultNumber
+}
+const resultCheck = checkNumber(78)
+console.log(resultCheck)
 
 //---> Resposta c.:
-let checkEvenNumbers = (checkArray) => {
-    let evenNumbers
+let checkEvenNumbers = (array) => {
    for(let i = 0; i < array.length; i++){
-       evenNumbers = array[i] % 2
+     let evenNumbers = array[i] % 2
        if(evenNumbers === 0){
            pairs++
        }
@@ -136,5 +135,41 @@ let checkEvenNumbers = (checkArray) => {
    return pairs
 }
 let pairs = 0
-console.log(evenNumbers(array))
- 
+console.log(checkEvenNumbers(array))
+
+//---> Resposta d.:
+let checkNumber = (number) => {
+    let resultNumber
+    for(let i = 0; i < array.length; i++){
+    if(number % 2 === 0){
+    resultNumber = true
+    } else{
+        resultNumber = false
+    }
+}
+  return resultNumber
+}
+const resultCheck = checkNumber(array)
+console.log(resultCheck)
+
+
+
+//DESAFIO - 1
+let printParameter = (parameter) =>{
+    console.log(parameter)
+}
+
+let sumParameters = (parameter1, parameter2) =>{
+    sum (parameter1, parameter2)
+
+    console.log(printParameter(sum))
+}
+
+
+
+
+
+
+
+
+
