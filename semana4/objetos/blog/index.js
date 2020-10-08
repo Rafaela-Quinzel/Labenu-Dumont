f45b06c733bbacd58fb98fe39a87b603eaa760b3
Rafaela-Quinzel <71155947+Rafaela-Quinzel@users.noreply.github.com>
2020-10-07 23:55:16 -0300

    Atividade de Objetos e desafios 1(inserir imagem) e 2(responsividade) concluídos

--- semana4/objetos/blog/index.js
+++ semana4/objetos/blog/index.js
@@ -1,30 +1,50 @@
 //EXERCÍCIOS:
 const pressButton = () => {
 
-const titleField =  document.getElementById("title-post")
-const authorField = document.getElementById("author-post")   //---> exercício 2
-const contentField = document.getElementById("content-post")
+    const titleField =  document.getElementById("title-post")
+    const authorField = document.getElementById("author-post")   //---> exercício 2
+    const contentField = document.getElementById("content-post")
+    const imageField = document.getElementById("image-post")
 
-const post = {
-    title: titleField.value,
-    author: authorField.value,
-    content: contentField.value
+
+    const post = {
+        title: titleField.value,
+        author: authorField.value,
+        content: contentField.value,
+        image: imageField.value
+    }
+
+    let arrayObjects = []  
+    arrayObjects.push(post)  //---> exercício 3
+    console.log(arrayObjects)
+
+    titleField.value = ""
+    authorField.value = ""
+    contentField.value = ""
+
+
+    const container = document.getElementById('container-de-posts') 
+        container.innerHTML += "<h1>" + post.title + "</h1>" //---> exercício 4 e 5
+        container.innerHTML += "<h3>" + post.author + "</h3>"
+        container.innerHTML += "<p>" + post.content + "</p>"
+        container.innerHTML += `<img class="img" src="${imageField.value}">`
+
+
+    //---> DESAFIO 1 - adicionar imagem
+    if(imageField.value.includes("http")){
+        imageField.value = ""  
+    } else{
+        window.alert("Favor inserir o link da imagem.")
+    }
+
 }
 
-let arrayObjects = []  
-arrayObjects.push(post)  //---> exercício 3
-console.log(arrayObjects)
+//---> DESAFIO 2 - Está no arquivo responsive.css
 
-titleField.value = ""
-authorField.value = ""
-contentField.value = ""
 
-const container = document.getElementById('container-de-posts') 
-container.innerHTML += "<h1>" + post.title + "</h1>" //---> exercício 4 e 5
-container.innerHTML += "<h3>" + post.author + "</h3>"
-container.innerHTML += "<p>" + post.content + "</p>"
-    
-}
+
+
+
 
 
 
