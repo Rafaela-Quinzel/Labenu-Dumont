3debff37b003ad2413666ad38474db9a7d49c1b9
Rafaela-Quinzel <71155947+Rafaela-Quinzel@users.noreply.github.com>
2020-10-09 11:32:55 -0300

    Interpretação de código: exercício 1 concluído

---
+++ semana4/lista-de-exercicios/exercicios.js
@@ -0,0 +1,20 @@
+//------------------------------------LISTA DE EXERCÍCIOS------------------------------------//
+
+//Exercícios de interpretação de código:
+
+// Exercício 01 //
+/*function conversorDeMoeda(valorEmDolar){
+    const cotacao = Number(prompt("Informe o valor da cotação do Dólar")) //---> Está variável está solicitando ao usuário que digite um número e vai guardar esse valor.
+
+    return "R$" + (valorEmDolar * cotacao) //---> Vai retornar R$ + o parâmetro da função * resultado do número digitado pelo usuário na variável cotacao, ex: R$500.
+}
+  const meuDinheiro = conversorDeMoeda(100) //---> Foi criado outra variável que vai armazenar a função com o novo valor de parâmetro(100). Agora valorEMDolar vale 100.
+  console.log(meuDinheiro) //---> Está imprimindo a variável meuDinheiro que, está pegando a função que traz o valor digitado * o valor que seu parâmetro tem dentro desta variável.
+  */
+                            
+
+
+
+
+
+
