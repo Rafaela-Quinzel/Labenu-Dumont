263dd851c6fa259d1732ae500db77f3468fcc963
Rafaela-Quinzel <71155947+Rafaela-Quinzel@users.noreply.github.com>
2020-10-09 12:32:39 -0300

    Interpretação de código: exercício 2 concluído

--- semana4/lista-de-exercicios/exercicios.js
+++ semana4/lista-de-exercicios/exercicios.js
@@ -11,6 +11,22 @@
   const meuDinheiro = conversorDeMoeda(100) //---> Foi criado outra variável que vai armazenar a função com o novo valor de parâmetro(100). Agora valorEMDolar vale 100.
   console.log(meuDinheiro) //---> Está imprimindo a variável meuDinheiro que, está pegando a função que traz o valor digitado * o valor que seu parâmetro tem dentro desta variável.
   */
+
+// Exercício 02 //
+/* -> Na primeira linha foi criada uma função, a investeDinheiro tem dois parâmetros: tipoDeInvestimento e valor.
+   -> Na segunda linha foi criada uma variavél valorAposInvestimento e sem nenhum valor atribuído.
+   -> Na quarta linha foi criado um switch com uma condição que traz o parâmetro tipoDeInvestimento que será comprada.
+   -> Dentro do switch ha cases que vão indicar as condições. Se a variável valorAposInvestimento for igual o que está dentro do case, o código (ex: valorAposInvestimento = valor * 1.03) vai se executado.
+   -> No final do switch tem o default que será executado, caso o valor esteja incorreto, exibindo um alert com uma mensagem:"TIPO DE INVESTIMENTO INFORMADO INCORRETO".
+   -> Na linha 21 vai retornar a variável valorAposInvestimento com seu novo valor conforme executado no switch.
+   -> Na linha 24 foi criado uma variável novoMontante que traz a função investeDinheiro com novos parâmetros("Ações", 150).
+   -> Na linha 25 foi criado uma outra variável chamada segundoMontante que também traz a função investeDinheiro com novos parâmetros("Tesouro Direto", 200).
+   -> Na linha 27 vai ser impresso a variável novoMontante. Vai ser verificado dentro do switch da função investeDinheiro caso seja "Ações", o que será executado.
+      No caso irá executar: valorAposInvestimento(variável) = "Ações"(parâmetro 1) * 1.1(parâmetro 2) - 165.
+   -> Na linha 28 vai ser impresso a variável segundoMontante. Vai ser verificado dentro do switch da função investeDinheiro caso seja "Tesouro Direto", o que será executado.
+      No caso irá executar o default com a mensagem:"TIPO DE INVESTIMENTO INFORMADO INCORRETO". Pois foi informado um parâmetro que não está declarado no switch como válido.
+*/
+
                             
 
 
