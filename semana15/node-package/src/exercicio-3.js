4f8c06617c645f00b281a8ed71c4d0f787b700a3
Rafaela-Quinzel <71155947+Rafaela-Quinzel@users.noreply.github.com>
2021-01-04 18:42:43 -0300

    Fiz mais ulguns ajustes

--- semana15/node-package/src/exercicio-3.js
+++
@@ -1,5 +0,0 @@
-const lista = ["fazer atividade do curso", "treinar boxe"]
-
-lista.push(process.argv[2])
-
-console.log(lista)
--- semana15/node-package/src/exercicio1/index.js
+++ semana15/node-package/src/exercicio1/index.js
@@ -1,8 +1,45 @@
-const userInfo = require("../userInfo.js") 
+// EXERCÍCIO 01 //
 
-const name = userInfo[0].name
+    /*
+    a)  Como fazemos para acessar os parâmetros passados na linha de comando para o Node?
+        Para acessar os parâmetros utilizamos o process.argv, nele colocamos qual a posição do parâmetro.
+        Já no terminal chamamos node nome-do-arquivo argumento ou npm nome-do-script argumento
+    */
 
-const age = Number(userInfo.age)
 
 
-console.log(`Olá, ${name}! Você tem ${age} anos.`);
+    /*
+    b) Crie um programa que receba seu nome e sua idade. 
+    Após receber estes valores, imprima no console uma mensagem que siga a seguinte estrutura:
+
+    "Olá, (Nome)! Você tem (sua idade) anos."
+
+
+        const userInfo = require("./userInfo.js") 
+
+        const name = process.argv[2]
+
+        const age = process.argv[3]
+
+
+        console.log(`Olá, ${name}! Você tem ${age} anos.`);
+    */
+
+
+    /*
+    c) Altere o programa acima para que mostre também a sua idade daqui a sete anos.
+
+    "Olá, (Nome)! Você tem (sua idade) anos. Em sete anos você terá (nova idade)"
+
+    */
+
+        const userInfo = require("../../src/user-info.js") 
+
+        const name = process.argv[2]
+
+        const age = process.argv[3]
+
+        const ageInSevenYears = Number(age) + 7
+
+
+        console.log(`Olá, ${name}! Você tem ${age} anos. Em sete anos você terá ${ageInSevenYears}`);
