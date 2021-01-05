6cefe795dc98cc10c89566b7fddfbad0c1b88e18
Rafaela-Quinzel <71155947+Rafaela-Quinzel@users.noreply.github.com>
2021-01-05 17:20:33 -0300

    Introdução ao Typescript = exercicio 02 concluído

--- semana15/first-script/src/exercicio1.ts
+++ semana15/first-script/src/exercicio1.ts
@@ -6,12 +6,14 @@
         const minhaString: string = "Olá!" // ---> Da erro, pois ele espera um valor tipo string.
 
 
+
     /*b) Crie uma variável meuNumero do tipo number e atribua um valor numérico. 
         Como podemos fazer para que essa variável também aceite strings? */
 
         //const meuNumero: number = 2 // ---> Da erro, pois ele espera um valor tipo number.
         const meuNumero: number | string = 2 // ---> Mas se colocar desta forma ele aceita outros tipos.
 
+        
 
     /* c) Agora crie um novo objeto. Este objeto é uma pessoa, e deve possuir três propriedades:
 
@@ -26,6 +28,9 @@
             idade: 31,
             corFavorita: "preto"
         }
+
+
+
     /* d) Crie mais três objetos, que também precisam ter apenas os campos definidos acima. 
         Crie um tipo Pessoa para garantir que todos os objetos tenham os mesmos campos.
                               
@@ -58,55 +63,10 @@
         }
 
         const jacira: person = {
-            nome: "Adriano",
+            nome: "Jacira",
             idade: 30,
             corFavorita: coresArcoIris.LARANJA
         }
 
 
 
-//--------------------------------------------- EXERCÍCIO 02 ----------------------------------------------------------//
-
-
-    /* a) Quais são as entradas e saídas dessa função? 
-    Copie a função para um arquivo .ts e faça a tipagem desses parâmetros */
-
-
-        function obterEstatisticas(numeros: number[]) {
-
-            const numerosOrdenados: number[] = numeros.sort(
-                (a, b) => a - b
-            )
-
-            let soma = 0
-
-            for (let num of numeros) {
-                soma += num
-            }
-
-            const estatisticas = {
-                maior: numerosOrdenados[numeros.length - 1],
-                menor: numerosOrdenados[0],
-                media: soma / numeros.length
-            }
-
-            return estatisticas
-        }
-
-
-
-    /* b) Que outras variáveis compõem essa função? Explicite a tipagem de todas elas */
-
-    // As variáveis de numerosOrdenados (tipagem: number) e soma (tipegem: number)
-
-
-
-    /* c) Crie um type para representar uma amostra de dados, isto é, um objeto com as chaves numeros e obterEstatisticas. 
-        Abaixo, temos um exemplo de objeto desse tipo, declarado em JavaScript: 
-        
-            const amostraDeIdades = {
-
-		    numeros: [21, 18, 65, 44, 15, 18],
-
-		    obterEstatisticas: (numeros) => {...}
-        } */
