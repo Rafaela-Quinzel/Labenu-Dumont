db521eadd0b575e03449d4866f113249ac634db5
Rafaela-Quinzel <71155947+Rafaela-Quinzel@users.noreply.github.com>
2021-01-07 13:28:54 -0300

    APIs Rest = exercício 01 - teóricas concluído

---
+++ semana15/template-apis-rest/src/perguntas-teoricas.ts
@@ -0,0 +1,15 @@
+//----------------------------------- EXERCÍCIO 01 -----------------------------------------------------//
+
+ /* Vamos começar fazendo um endpoint que busque todos os usuários de uma lista. */
+
+    /* a) Qual método HTTP você deve utilizar para isso?
+    
+        R: Devemos utilizar o método GET, que vai retornar toda a lista de usuários.
+
+    */
+
+    /* b) Vamos começar fazendo um endpoint que busque todos os usuários de uma lista. 
+    
+       R: A entidade que está sendo manipulada é "user".
+    
+    */
