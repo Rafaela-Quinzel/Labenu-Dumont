f102cb3b41215cd14f2172e8e4562338a2d784d8
Rafaela-Quinzel <71155947+Rafaela-Quinzel@users.noreply.github.com>
2021-01-13 16:18:14 -0300

    Knex -> exercício 03 concluído

--- semana16/knex/src/index.ts
+++ semana16/knex/src/index.ts
@@ -27,6 +27,60 @@ const connection: Knex = knex({
  })
 
 
+// EXERCÍCIO 03 
+// a) 
+async function getActorById(id: string): Promise<any> {
+    const result = await connection.raw(`
+       SELECT * FROM Actor WHERE id = '${id}'
+    `)
+
+    return result[0]
+}
+getActorById("")
+
+
+app.get("/actor/:id", async (req: Request, res: Response) => {
+    try {
+      const id = req.params.id
+      const actor = await getActorById(id)
+  
+      res.status(200).send(actor)
+    } catch (error) {
+      res.status(400).send({
+        message: error.message,
+      });
+    }
+})
+
+
+
+// EXERCÍCIO 03 
+// b) 
+async function countActorByGender(gender: string): Promise<any> {
+    const result = await connection.raw(`
+       SELECT COUNT(*) as count FROM Actor WHERE gender = '${gender}'
+    `)
+    console.log(result[0][0].count)
+    return result[0][0].count
+}
+countActorByGender("female")
+
+
+app.get('/actor', async (req:Request , res:Response) => {
+    try {
+       const gender = req.query.gender;
+       const count = await countActorByGender(gender as string);
+       res.status(200).send({
+          quantity: count,
+        })
+    } catch (error) {
+       res.status(400).send({
+         message: error.message,
+       });
+     }
+})
+
+
 
 
 
