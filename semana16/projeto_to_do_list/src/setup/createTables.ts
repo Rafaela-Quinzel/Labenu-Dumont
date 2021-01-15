6d3d6a706215c33f3acf2e86b7df0ea43428ddd7
Rafaela-Quinzel <71155947+Rafaela-Quinzel@users.noreply.github.com>
2021-01-15 16:26:42 -0300

    Arquivo de criar tabelas criado

--- semana16/projeto_to_do_list/src/setup/createTables.ts
+++ semana16/projeto_to_do_list/src/setup/createTables.ts
@@ -1,11 +1,43 @@
-import express, { Express, Request, Response } from 'express'
-import cors from 'cors'
-import { AddressInfo } from 'net'
-import knex from 'knex'
-import Knex from 'knex'
-import dotenv from 'dotenv'
 import { connection } from '../connections/dataBaseConnection'
-import { User } from '../types/users'
 
 
 
+const createTable = async(): Promise<void> => {
+
+    await connection.raw(`
+      
+        CREATE TABLE ToDoList_User (
+          id VARCHAR(255) PRIMARY KEY,
+          name VARCHAR(255) NOT NULL,
+          nickname VARCHAR(255) NOT NULL,
+          email: VARCHAR(255) UNIQUE NOT NULL,
+        );    
+    
+    `)
+
+    await connection.raw(`
+      
+        CREATE TABLE ToDoList_Task (
+          taskId VARCHAR(255) PRIMARY KEY,
+          title VARCHAR(255) NOT NULL,
+          description TEXT NOT NULL,
+          limitDate: DATE NOT NULL,
+          status:  VARCHAR(255) NOT NULL DEFAULT "to_do",
+          creatorUserId: VARCHAR(255) NOT NULL,
+          FOREIGN KEY (creatorUserId) REFERENCES ToDoListUSer(id)
+        );    
+    
+    `)
+
+
+    await connection.raw(`
+      
+        CREATE TABLE Accountable_Task (
+          taskId VARCHAR(255),
+          userId VARCHAR(255),
+          FOREIGN KEY (taskId) REFERENCES ToDoList_Task(id)
+          FOREIGN KEY (userId) REFERENCES ToDoList_USer(id)
+        );    
+    
+    `)
+} 
