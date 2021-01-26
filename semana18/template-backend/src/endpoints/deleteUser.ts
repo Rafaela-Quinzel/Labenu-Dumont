2100a0738269bace57edf85e66f50e69f0e65c51
Rafaela-Quinzel <71155947+Rafaela-Quinzel@users.noreply.github.com>
2021-01-26 18:28:10 -0300

    Criptografia -> exercício 05 concluído

---
+++ semana18/template-backend/src/endpoints/deleteUser.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from 'express'
+import { AuthenticationData } from '../types/authenticationData'
+import { getTokenData } from '../services/authenticator'
+import { USER_ROLES } from '../types/user'
+import delUser from '../data/delUser'
+
+
+
+
+export default async function deleteUser (req: Request, res: Response) {
+    
+    try {
+
+        const id: string = req.params.id as string
+
+        const token: string = req.headers.authorization as string
+
+        const tokenData: AuthenticationData = getTokenData(token)
+
+        if (tokenData.role !== USER_ROLES.ADMIN) {
+
+            res.statusCode = 401
+
+            throw new Error('Funcionalidade permitida somente para usuários "ADMIN"!')
+        }
+
+        await delUser(id)
+
+        res.status(200).send("Usuário deletado com sucesso!")
+
+    } catch (error) {
+
+        res.send({
+
+            message: error.message || error.sqlMessage
+        })
+    }
+}
