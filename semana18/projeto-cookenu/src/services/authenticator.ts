f3d15d05a9daf164fb18a7a188cc7b8c5e52c11c
Rafaela-Quinzel <71155947+Rafaela-Quinzel@users.noreply.github.com>
2021-01-28 23:04:02 -0300

    Fiz uma pequena alteração

--- semana18/projeto-cookenu/src/services/authenticator.ts
+++ semana18/projeto-cookenu/src/services/authenticator.ts
@@ -3,9 +3,7 @@ import dotenv from 'dotenv'
 import { AuthenticationData } from '../types/authenticationData'
 
 
-
 dotenv.config()
-
 
 export function generateToken(input: AuthenticationData): string {
 
@@ -22,7 +20,7 @@ export function generateToken(input: AuthenticationData): string {
 }
 
 export function getTokenData(token: string): AuthenticationData {
-    const payload = jwt.verify(token, process.env.JWT_KEY as string) as any
+    const payload = jwt.verify(token, process.env.JWT_KEY as string) as AuthenticationData
 
     return {
         id: payload.id,
