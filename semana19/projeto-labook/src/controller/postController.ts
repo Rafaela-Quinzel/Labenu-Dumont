3c96ced2eb739a2109d4577b3c91d1a7cbe8105d
Rafaela-Quinzel <71155947+Rafaela-Quinzel@users.noreply.github.com>
2021-02-03 18:25:02 -0300

    Endpoint getPostById roganizado

--- semana19/projeto-labook/src/controller/postController.ts
+++ semana19/projeto-labook/src/controller/postController.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from 'express'
 import { AuthenticationData } from '../business/entities/user'
-import { businessCreatePost } from '../business/postBusiness'
+import { businessCreatePost, businessGetPostById } from '../business/postBusiness'
 import { getTokenData } from '../business/services/authenticator'
+import { selectPostById } from '../data/postDatabase'
 
 
 
@@ -35,6 +36,26 @@ export const createPost = async (req: Request, res: Response) => {
     } catch (error) {
   
       res.status(400).send(error.message)
+    }
+}
+
+
+export const getPostById = async (req: Request, res: Response) => {
+
+    try {
+
+        const id = req.params.id 
+    
+        const result = await selectPostById(id)
+    
+        await businessGetPostById(result)
+    
+        res.status(200).send(result)
+    
+    } catch (error) {
+    
+        res.status(400).send(error.message)
+    
     }
 }
   
