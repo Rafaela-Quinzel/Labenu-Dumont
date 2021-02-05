dd72cd29133dca8ad6a7e8cc5d6f9789ac8c8194
Rafaela-Quinzel <71155947+Rafaela-Quinzel@users.noreply.github.com>
2021-02-05 14:33:26 -0300

    Arquivos postController e postBusiness alterados para classes

--- semana19/projeto-labook/src/business/postBusiness.ts
+++ semana19/projeto-labook/src/business/postBusiness.ts
@@ -4,65 +4,68 @@ import { AuthenticationData } from "./entities/user"
 import { getTokenData } from "./services/authenticator"
 import { generateId } from "./services/idGenerator"
 
-
-
 const postDatabase: PostDatabase = new PostDatabase()
 
-export const businessCreatePost = async (input: any) => {
+export class PostBusiness {
 
 
-    if (!input.photo) {
+    businessCreatePost = async (input: any) => {
 
-        throw new Error("Photo not informed!")
+
+        if (!input.photo) {
+
+            throw new Error("Photo not informed!")
+        }
+
+        if (!input.description) {
+
+            throw new Error("Description not informed!")
+        }
+    
+    
+        if (input.type !== POST_TYPES.NORMAL && input.type !== POST_TYPES.EVENT) {
+    
+            throw new Error(`Please fill in a type valid:"normal" or "event"`)
+        }
+    
+
+        const tokenData: AuthenticationData = getTokenData(input.token)
+
+        const id: string = generateId()
+
+        let today = Date.now()
+        let dayjs = require('dayjs')
+        today = dayjs(today, 'x').format('DD/MM/YYYY')
+
+        const newPost = {
+            id: id,
+            photo: input.photo,
+            description: input.description,
+            type: input.type,
+            created_at: input.today,
+            author_id: tokenData.id
+        }
+
+        await postDatabase.insertPost(newPost)
+    
     }
 
-    if (!input.description) {
 
-        throw new Error("Description not informed!")
+    businessGetPostById = async (id: string) => {
+
+        const result = await postDatabase.selectPostById(id)
+
+        if (!result) {
+
+            throw new Error("Post not found!")
+
+        } else {
+
+            return result
+        }
+    
+
     }
- 
- 
-    if (input.type !== POST_TYPES.NORMAL && input.type !== POST_TYPES.EVENT) {
- 
-        throw new Error(`Please fill in a type valid:"normal" or "event"`)
-    }
- 
-
-    const tokenData: AuthenticationData = getTokenData(input.token)
-
-    const id: string = generateId()
-
-    let today = Date.now()
-    let dayjs = require('dayjs')
-    today = dayjs(today, 'x').format('DD/MM/YYYY')
-
-    const newPost = {
-        id: id,
-        photo: input.photo,
-        description: input.description,
-        type: input.type,
-        created_at: input.today,
-        author_id: tokenData.id
-    }
-
-    await postDatabase.insertPost(newPost)
- 
-}
-
-
-export const businessGetPostById = async (id: string) => {
-
-    const result = await postDatabase.selectPostById(id)
-
-      if (!result) {
-
-        throw new Error("Post not found!")
-
-      } else {
-
-        return result
-    }
- 
 
 }
  
--- semana19/projeto-labook/src/controller/postController.ts
+++ semana19/projeto-labook/src/controller/postController.ts
@@ -1,59 +1,65 @@
 import { Request, Response } from 'express'
 import { postInputDTO } from '../business/entities/post'
-import { businessCreatePost, businessGetPostById } from '../business/postBusiness'
+import { PostBusiness } from '../business/postBusiness'
 import { PostDatabase } from '../data/postDatabase'
 
 
 
 const postDatabase: PostDatabase = new PostDatabase()
 
-export const createPost = async (req: Request, res: Response) => {
+const postBusiness: PostBusiness = new PostBusiness()
 
-  try {
-  
-    const { photo, description, type } = req.body
+export class PostController {
+
+  createPost = async (req: Request, res: Response) => {
+
+    try {
+    
+      const { photo, description, type } = req.body
 
 
-    const token: string = req.headers.authorization as string
+      const token: string = req.headers.authorization as string
 
 
-    const postData: postInputDTO = {
-      token, 
-      photo,
-      description, 
-      type
+      const postData: postInputDTO = {
+        token, 
+        photo,
+        description, 
+        type
+      }
+
+          
+      await postBusiness.businessCreatePost(postData)
+      
+      res.status(200).send({message: "Post successfully created!"})
+    
+    } catch (error) {
+    
+      res.status(400).send(error.message)
+
     }
 
-        
-    await businessCreatePost(postData)
-    
-    res.status(200).send({message: "Post successfully created!"})
-  
-  } catch (error) {
-  
-    res.status(400).send(error.message)
-
   }
 
-}
 
+  getPostById = async (req: Request, res: Response) => {
 
-export const getPostById = async (req: Request, res: Response) => {
+    try {
 
-  try {
-
-    const id = req.params.id 
-    
-    const result = await postDatabase.selectPostById(id)
-    
-    await businessGetPostById(result)
-    
-    res.status(200).send(result)
-    
-  } catch (error) {
-    
-    res.status(400).send(error.message)
-    
+      const id = req.params.id 
+      
+      const result = await postDatabase.selectPostById(id)
+      
+      await postBusiness.businessGetPostById(result)
+      
+      res.status(200).send(result)
+      
+    } catch (error) {
+      
+      res.status(400).send(error.message)
+      
+    }
   }
+
 }
   
