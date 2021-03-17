9bf591fc3f8079f5458d44223c139e8a840ecc44
Rafaela-Quinzel <71155947+Rafaela-Quinzel@users.noreply.github.com>
2021-03-17 15:56:19 -0300

    Exercício 03

--- semana24/recursividade/src/recursive/Exercicio02.ts
+++ semana24/recursividade/src/recursive/Exercicio02.ts
@@ -2,6 +2,5 @@ const sumNumbers = (intereger: number): number => {
     if (intereger > 0) {
         return intereger + sumNumbers(intereger - 1)
     } else return 0
-  }
-  console.log(sumNumbers(5))
-  
+}
+console.log(sumNumbers(5))
