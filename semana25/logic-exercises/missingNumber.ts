78760895c37d7c913c8efa554536c220c1da9c1e
Rafaela-Quinzel <71155947+Rafaela-Quinzel@users.noreply.github.com>
2021-03-22 13:44:43 -0300

    Exercício do dia

---
+++ semana25/logic-exercises/missingNumber.ts
@@ -0,0 +1,16 @@
+export const findMissingNumber = (array: number[]): number | void => {
+    const sortedArray: Array<number | undefined> = []
+
+    for (const num of array) {
+        sortedArray[num - 1] = num
+    }
+
+    for (let i = 0; i < sortedArray.length; i++) {
+        if (sortedArray[i] === undefined) return i + 1
+    }
+
+    if (sortedArray[array.length - 1] !== 100) {
+        return 100
+    } 
+}
+console.log(findMissingNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
