4e6348301377292a3af534c606b0283ed0613ed7
Rafaela-Quinzel <71155947+Rafaela-Quinzel@users.noreply.github.com>
2021-03-30 10:40:41 -0300

    Exercício do dia

---
+++ semana26/anagram/src/isAnagram.ts
@@ -0,0 +1,6 @@
+function isAnagram(word1: string, word2: string) {
+    return word1.split("").sort().join("") === word2.split("").sort().join("")
+}
+console.log(isAnagram("gato", "toga")) //true
+console.log(isAnagram("gato", "rato")) //false
+console.log(isAnagram("gato", "togas")) //false
